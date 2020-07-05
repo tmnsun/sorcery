82b63e9a97f33359256754f80c3ee49695dc02a6
Andrey Filippov <tmn.sun@gmail.com>
2020-07-05 19:00:17 +0300

    Fix VK login

--- lib/sorcery/providers/vk.rb
+++ lib/sorcery/providers/vk.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module Sorcery
   module Providers
     # This class adds support for OAuth with vk.com.
@@ -7,7 +9,6 @@ module Sorcery
     #   ...
     #
     class Vk < Base
-
       include Protocols::Oauth2
 
       attr_accessor :auth_path, :token_path, :user_info_url, :scope
@@ -27,9 +28,10 @@ module Sorcery
 
         params = {
           access_token: access_token.token,
-          uids:         access_token.params['user_id'],
-          fields:       user_info_mapping.values.join(','),
-          scope:        scope
+          uids: access_token.params['user_id'],
+          fields: user_info_mapping.values.join(','),
+          scope: scope,
+          v: '5.21'
         }
 
         response = access_token.get(@user_info_url, params: params)
@@ -37,7 +39,7 @@ module Sorcery
           user_hash[:user_info] = user_hash[:user_info]['response'][0]
           user_hash[:user_info]['full_name'] = [user_hash[:user_info]['first_name'], user_hash[:user_info]['last_name']].join(' ')
 
-          user_hash[:uid] = user_hash[:user_info]['uid']
+          user_hash[:uid] = user_hash[:user_info]['id']
           user_hash[:user_info]['email'] = access_token.params['email']
         end
         user_hash
@@ -45,19 +47,18 @@ module Sorcery
 
       # calculates and returns the url to which the user should be redirected,
       # to get authenticated at the external provider's site.
-      def login_url(params, session)
-        self.authorize_url({ authorize_url: auth_path })
+      def login_url(_params, _session)
+        authorize_url({ authorize_url: auth_path })
       end
 
       # tries to login the user from access token
-      def process_callback(params, session)
+      def process_callback(params, _session)
         args = {}.tap do |a|
           a[:code] = params[:code] if params[:code]
         end
 
         get_access_token(args, token_url: token_path, token_method: :post)
       end
-
     end
   end
 end
